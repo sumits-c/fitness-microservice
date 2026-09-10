0a344d60537b10ae7819d33d982225214a2f419b
Sumit Chauhan <sumitchauhan1080@gmail.com>
2026-09-10 15:48:20 +0530

    Initial commit

---
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -0,0 +1,56 @@
+package com.fitness.userservice.service;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.model.User;
+import com.fitness.userservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class UserService {
+    private UserRepository repository;
+    public UserResponse register(RegisterRequest request){
+        if(repository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Email already exist");
+        }
+        User user= new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        User savedUser = repository.save(user);
+        UserResponse userResponse=new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+        return userResponse;
+    }
+
+    public UserResponse getUserProfile(String userId) {
+        User user = repository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User Not Found"));
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.getId());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        userResponse.setUpdatedAt(user.getUpdatedAt());
+        return userResponse;
+    }
+
+    public Boolean existsByUserId(String userId) {
+        log.info("Calling user validation API for userId:{}",userId);
+        return repository.existsById(userId);
+    }
+}
